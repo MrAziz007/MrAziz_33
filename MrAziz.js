84aa62dd39718e522430d0ce2d28980a64e3eab3
MrAziz007 <isido7094@gmail.com>
2024-05-04 17:11:58 +0500

    Методы массивов продолжение

---
+++ MrAziz.js
@@ -0,0 +1,16 @@
+// let arr = ['peach', 'Apple', 'banana', 'apricot', 'kiwi', 'orange', 'ananas'];
+// arr.push(prompt('Вбейте рандомное слово'));
+// console.log(arr.sort());
+// console.log(arr.join("").length);
+
+// let num1 = [3, 6, [7, 5]];
+// let num2 = [2, 9, [1, [4], 8]];
+// num1 = num1.flat().concat(num2.flat().flat());
+// console.log(num1.sort());
+
+// let numbers = [23, 44, 'Alex', 'John', 3, 23, 10];
+// let names = ['Stark', 'Tyler', 'Adams'];
+// names.push(numbers.splice(2, 2));
+// names = names.flat().sort();
+// numbers = numbers.sort();
+// if (names.includes('Constantin') == true) { } else { names.push('Constantin') };
